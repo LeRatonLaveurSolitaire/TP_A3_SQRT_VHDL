407e20c4fca75010589f6662f0cc7156b9663bc9
LeRaton <49453331+LeRatonLaveurSolitaire@users.noreply.github.com>
2025-01-08 23:09:10 +0100

    Fast sqrt and beginning of smal sqrt

---
+++ simple_adder.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity generic_adder is
+    generic (
+        N : integer := 32
+    );
+    port (
+        a, b : in std_logic_vector(N-1 downto 0);
+        sum : out std_logic_vector(N-1 downto 0);
+        carry_out : out std_logic
+    );
+end generic_adder;
+
+architecture Behavioral of generic_adder is
+    signal temp_sum : unsigned(N downto 0);
+begin
+    temp_sum <= ('0' & unsigned(a)) + ('0' & unsigned(b));
+    sum <= std_logic_vector(temp_sum(N-1 downto 0));
+    carry_out <= temp_sum(N);
+end Behavioral;
---
+++ sqrt_small.vhd
@@ -0,0 +1,55 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sqrt is
+    generic(
+        n : integer :=32 -- nombre de bits des entrées de sorties
+    );
+    port(
+        CLK : in STD_LOGIC; -- clock
+        RST : in STD_LOGIC; -- Démarre le calcul sur front descendant de RST
+        A : in STD_LOGIC_VECTOR(2*n -1 downto 0); -- nombre entier non signé dont on veut calculer la racine carrée
+        Z_OUT : out STD_LOGIC_VECTOR(n -1 downto 0); -- racine carrée de A
+        READY : out STD_LOGIC; -- = 1 lorsque le calcul est fini
+    );
+end entity sqrt;
+
+architecture arch of sqrt is
+    signal D : STD_LOGIC_VECTOR(2*n - 1 downto 0);
+    signal R : STD_LOGIC_VECTOR(2*n - 1 downto 0);
+    signal Z : STD_LOGIC_VECTOR(n - 1 downto 0);
+    signal CNT : integer;
+begin
+    sqrt_sequential: process(clk, rst)
+    begin
+        if RST = '1' then
+            D <= A;
+            R <= (others => '0');
+            Z <= (others => '0');
+            CNT <= 0;
+            READY <= '0';
+        elsif rising_edge(clk) and CNT<n then
+            if R>='0' then
+                R <= (R Sll 2) + (D Srl (2n - 2)) - ((Z+1) Sll 2 );
+            else
+                R <= (R Sll 2) + (D Srl (2n - 2)) + ((Z+3) Sll 2 );
+            end if;
+
+            if R>= '0' then
+                Z <= (Z sll 1) + 1
+            else 
+                Z <= (Z sll 1)
+            end if;
+            D <= D Sll 2;
+            CNT <= CNT + 1;
+        elsif CNT = n then
+            Z_OUT <= Z ;
+            READY <='1';
+        end if;
+    end process sqrt_sequential;
+    
+    
+end architecture arch;
+
+
