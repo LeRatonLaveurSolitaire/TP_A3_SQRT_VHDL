15ce79e2e27a625d02121b3e613fe9edd2f509e3
LeRaton <thomasboulanger2503@gmail.com>
2025-02-06 12:39:26 +0100

    SQRT in C

--- Nios/nios_system/synthesis/submodules/nios_system_sysid.v
+++ Nios/nios_system/synthesis/submodules/nios_system_sysid.v
@@ -36,7 +36,7 @@ module nios_system_sysid (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1738232517 : 0;
+  assign readdata = address ? 1738841090 : 0;
 
 endmodule
 
